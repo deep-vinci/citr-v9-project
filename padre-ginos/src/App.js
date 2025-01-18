fcc408994c06da3c0ba917c025790c39e68bba11
Deepak <deepakworkpc@gmail.com>
2025-01-18 19:10:19 +0530

    chore: add prettier formatter

--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -1,30 +1,30 @@
 const Pizza = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.key),
-        React.createElement("p", {}, props.description),
+    return React.createElement('div', {}, [
+        React.createElement('h1', {}, props.name),
+        React.createElement('p', {}, props.description)
     ]);
 };
 
 const App = () => {
     return React.createElement(
-        "div",
+        'div',
         {},
-        React.createElement("h1", {}, "Padre Gino's"),
+        React.createElement('h1', {}, "Padre Gino's"),
         React.createElement(Pizza, {
-            name: "The Pepperoni Pizzza",
-            description: "Mozzarella Cheese, Pepperoni",
+            name: 'The Pepperoni Pizzza',
+            description: 'Mozzarella Cheese, Pepperoni'
         }),
         React.createElement(Pizza, {
-            name: "The Hawaiian Pizza",
-            description: "Sliced Ham, Pineapple, Mozzarella Cheese",
+            name: 'The Hawaiian Pizza',
+            description: 'Sliced Ham, Pineapple, Mozzarella Cheese'
         }),
         React.createElement(Pizza, {
-            name: "The Big Meat Pizza",
-            description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+            name: 'The Big Meat Pizza',
+            description: 'Bacon, Pepperoni, Italian Sausage, Chorizo Sausage'
         })
     );
 };
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(React.createElement(App));
